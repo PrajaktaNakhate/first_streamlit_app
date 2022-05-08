8f88adc82cd66a50a024f6c7e00bad28b01764d5
PrajaktaNakhate <87517799+PrajaktaNakhate@users.noreply.github.com>
2022-05-08 15:27:54 +0530

    Update streamlit_app.py
    imported pandas and did some changes under fruit smoothie menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit
+import pandas
+
 streamlit.title('My mom\'s new healthy diner')
 streamlit.header('🍽 Breakfast menu')
 streamlit.text('🍛 Omega 3 and blueberry oatmeal')
 streamlit.text('🌯 dosa')
 streamlit.text('🥚 hard boiled free-range egg')
 streamlit.header('🍌🍒 Build your own fruit smoothie 🍐🍏')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
 
