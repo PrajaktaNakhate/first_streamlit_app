32fd43e7de3562daa2682b0a2128953bd99015c6
PrajaktaNakhate <87517799+PrajaktaNakhate@users.noreply.github.com>
2022-05-13 13:46:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized= pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-my_cnx=snowflake.conector.connect(**streamlit.secrets["snowflake"])
+my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur=my_cnx.cursor()
 my_cur.execute()
 my_data_row=my_cur.fetchone()
